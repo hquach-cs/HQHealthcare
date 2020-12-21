7fc645ab302689b9f08501b8aa5e00d5f089e48f
hquach-cs <hquach.cs@gmail.com>
2020-12-21 17:10:50 -0600

    Header guide implemented

--- src/components/Header/HeaderElements.js
+++ src/components/Header/HeaderElements.js
@@ -23,7 +23,7 @@ export const HeaderWarningButton = styled.div`
   display: inline;
   border-radius: 5px;
   margin-left: 20px;
-  padding: 5px;
+  padding: 3px;
   cursor: pointer;
   transition: 0.2s all ease-in-out;
 
@@ -33,7 +33,7 @@ export const HeaderWarningButton = styled.div`
   }
 `;
 
-export const HeaderContainer = styled.div`
+export const HeaderWrapper = styled.div`
   width: 100%;
   height: 500px;
   background-image: url(${HeaderPic});
@@ -42,6 +42,10 @@ export const HeaderContainer = styled.div`
   background-position: center; /* Center the image */
   background-repeat: no-repeat; /* Do not repeat the image */
   background-size: cover; /* Resize the background image to cover the entire container */
+`;
+
+export const HeaderContainer = styled.div`
+  width: 100%;
 `;
 
 export const HeaderContainerText = styled.h2`
@@ -61,4 +65,58 @@ export const HeaderContainerTextInner = styled.h2`
   text-align: center;
   padding-top: 20px;
   font-weight: normal;
+`;
+
+export const HeaderGuide = styled.div`
+  width: 100%;
+  height: 200px;
+  background: red;
+`;
+
+export const HeaderGuideMenu = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const HeaderGuideItem = styled.li`
+  width: 200px;
+  height: 100%;
+  border: black;
+  background: #ee0000;
+  display: flex;
+  text-align: center;
+  flex-direction: column;
+  padding: 10px;
+  color: white;
+  align-items: center;
+  justify-content: space-evenly;
+  margin-left: 15px;
+  margin-right: 15px;
+`;
+
+export const HeaderGuideTitle = styled.h2`
+  font-size: 1.5rem;
+`;
+
+export const HeaderGuideDesc = styled.span``;
+
+export const HeaderGuideButton = styled.div`
+  background: white;
+  color: red;
+  width: 70%;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+`;
+
+export const HeaderGuideSeperator = styled.div`
+  height: 60%;
+  width: 1px;
+  border: 1px solid white;
 `;
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,6 +6,14 @@ import {
   HeaderWarningButton,
   HeaderContainerText,
   HeaderContainerTextInner,
+  HeaderWrapper,
+  HeaderGuide,
+  HeaderGuideMenu,
+  HeaderGuideItem,
+  HeaderGuideTitle,
+  HeaderGuideDesc,
+  HeaderGuideButton,
+  HeaderGuideSeperator,
 } from "./HeaderElements";
 
 const Header = () => {
@@ -19,14 +27,51 @@ const Header = () => {
         </HeaderWarningText>
       </HeaderWarning>
       <HeaderContainer>
-        <HeaderContainerText>
-          During tragic time, we will be here for you!
-          <span>
-            <HeaderContainerTextInner>
-              Life-changing and everyday care.
-            </HeaderContainerTextInner>
-          </span>
-        </HeaderContainerText>
+        <HeaderWrapper>
+          <HeaderContainerText>
+            During tragic time, we will be here for you!
+            <span>
+              <HeaderContainerTextInner>
+                24/7 Life-changing care.
+              </HeaderContainerTextInner>
+            </span>
+          </HeaderContainerText>
+        </HeaderWrapper>
+        <HeaderGuide>
+          <HeaderGuideMenu>
+            <HeaderGuideItem>
+              <HeaderGuideTitle>Doctors & Staffs</HeaderGuideTitle>
+              <HeaderGuideDesc>
+                All doctors and staff take upmost care for each individual.
+              </HeaderGuideDesc>
+              <HeaderGuideButton>Doctors</HeaderGuideButton>
+            </HeaderGuideItem>
+            <HeaderGuideSeperator />
+            <HeaderGuideItem>
+              <HeaderGuideTitle>Locations </HeaderGuideTitle>
+              <HeaderGuideDesc>
+                We are located in the heart of Nashville, Tennessee. To find
+                directions click below!
+              </HeaderGuideDesc>
+              <HeaderGuideButton>Directions</HeaderGuideButton>
+            </HeaderGuideItem>
+            <HeaderGuideSeperator />
+
+            <HeaderGuideItem>
+              <HeaderGuideTitle>Visitor Guide</HeaderGuideTitle>
+              <HeaderGuideDesc>
+                During coronavirus, rules and regulations has been changed.
+              </HeaderGuideDesc>
+              <HeaderGuideButton>Learn More</HeaderGuideButton>
+            </HeaderGuideItem>
+            <HeaderGuideSeperator />
+
+            <HeaderGuideItem>
+              <HeaderGuideTitle>Schedule an Appointment</HeaderGuideTitle>
+              <HeaderGuideButton>Appointment</HeaderGuideButton>
+            </HeaderGuideItem>
+          </HeaderGuideMenu>
+        </HeaderGuide>
       </HeaderContainer>
     </>
   );
