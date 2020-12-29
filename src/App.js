13f28ae7e5d046495ae9a0afbba6535d098b6be7
hquach-cs <hquach.cs@gmail.com>
2020-12-29 15:37:11 -0600

    starting over commit

--- src/App.js
+++ src/App.js
@@ -2,22 +2,10 @@ import React from "react";
 import { Route, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import SignIn from "./components/SignIn";
-import StorySection from "./components/StorySection";
 function App() {
   return (
     <Router>
       <Navbar />
-      <Route exact path="/">
-        <Header />
-        <StorySection />
-        <Footer />
-      </Route>
-      <Route path="/signin">
-        <SignIn />
-      </Route>
     </Router>
   );
 }
