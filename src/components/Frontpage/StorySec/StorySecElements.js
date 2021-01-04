77681dd32af2e4b9d5a5c1758572727817f1704b
hquach-cs <hquach.cs@gmail.com>
2021-01-04 14:29:11 -0600

    Story section w/ no pic done

--- src/components/Frontpage/StorySec/StorySecElements.js
+++ src/components/Frontpage/StorySec/StorySecElements.js
@@ -22,3 +22,46 @@ export const StorySectionDesc = styled.span`
   text-align: center;
   width: 60%;
 `;
+
+export const StoryList = styled.ul`
+  list-style: none;
+  display: flex;
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const StoryPic = styled.div`
+  border: 1px solid rgba(242, 242, 230, 1);
+`;
+
+export const StoryTitle = styled.h1`
+  font-size: 25px;
+  text-align: center;
+`;
+
+export const StoryDesc = styled.span`
+  font-size: 14px;
+  padding: 4px;
+  text-align: center;
+`;
+
+export const StoryButton = styled.a`
+  cursor: pointer;
+  text-align: center;
+  color: #4ba3c3;
+  font-size: 15px;
+  &:hover {
+    color: #cce6f4;
+  }
+`;
+
+export const Story = styled.li`
+  width: 300px;
+  height: 400px;
+  background: white;
+  margin: 10px;
+  border: 1px solid rgba(242, 242, 230, 1);
+  display: grid;
+  grid-template-rows: 50% 10% 25%;
+`;
--- src/components/Frontpage/StorySec/index.js
+++ src/components/Frontpage/StorySec/index.js
@@ -4,6 +4,12 @@ import {
   StorySection,
   StorySectionTitle,
   StorySectionDesc,
+  StoryList,
+  Story,
+  StoryTitle,
+  StoryPic,
+  StoryDesc,
+  StoryButton,
 } from "./StorySecElements";
 
 const StorySec = () => {
@@ -16,6 +22,45 @@ const StorySec = () => {
         Etiam nec diam euismod, elementum neque a, laoreet ipsum. Cras vel metus
         eu est sollicitudin egestas.
       </StorySectionDesc>
+      <StoryList>
+        <Story>
+          <StoryPic></StoryPic>
+          <StoryTitle>Vaccine Progression</StoryTitle>
+          <StoryDesc>
+            This article talks about the progression of the vaccine and the task
+            it takes to deliever to millions.
+          </StoryDesc>
+          <StoryButton>Learn More!</StoryButton>
+        </Story>
+        <Story>
+          <StoryPic></StoryPic>
+          <StoryTitle>New HC Mandate</StoryTitle>
+          <StoryDesc>
+            With the pandemic, the mandate of new policy takes in place. This
+            allows upmost saftey policy to be incorprated for better healthcare.
+          </StoryDesc>
+          <StoryButton>Learn More!</StoryButton>
+        </Story>
+        <Story>
+          <StoryPic></StoryPic>
+          <StoryTitle>Safety Policy</StoryTitle>
+          <StoryDesc>
+            Studies been done to showcase mask and washing hands help prevent
+            spread of COVID-19. Wearing mask also is a mandatory for in-person
+            safety.
+          </StoryDesc>
+          <StoryButton>Learn More!</StoryButton>
+        </Story>
+        <Story>
+          <StoryPic></StoryPic>
+          <StoryTitle>Upcoming 2021</StoryTitle>
+          <StoryDesc>
+            With the new year up ahead, new things are being tested and
+            implemented. Click below to know more.
+          </StoryDesc>
+          <StoryButton>Learn More!</StoryButton>
+        </Story>
+      </StoryList>
     </StorySection>
   );
 };
